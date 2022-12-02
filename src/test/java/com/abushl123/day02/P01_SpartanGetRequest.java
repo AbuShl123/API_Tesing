b82be13d6c4d0762ef6703cc7e28824a7c3ed5cf
Abubakr Shoaliev <shoalievabubakr@gmail.com>
2022-12-03 02:17:37 +0500

    this class is done.
    some more practices

--- src/test/java/com/abushl123/day02/P01_SpartanGetRequest.java
+++ src/test/java/com/abushl123/day02/P01_SpartanGetRequest.java
@@ -4,6 +4,7 @@ import com.abushl123.my_own.baseClasses.ApiTestConstants;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,6 +17,7 @@ public class P01_SpartanGetRequest extends ApiTestConstants {
      * And Content type should be application/json
      */
 
+    @DisplayName("GET all spartans")
     @Test
     public void get_all_spartans() {
         Response response = RestAssured
@@ -43,5 +45,58 @@ public class P01_SpartanGetRequest extends ApiTestConstants {
 
         System.out.println("response.headers().hasHeaderWithName(\"Transfer-Encoding\") = " +
                 response.headers().hasHeaderWithName("Transfer-Encoding"));
+    }
+
+
+    @DisplayName("GET single spartan")
+    @Test
+    public void getSpartan() {
+        Response response = RestAssured
+                .given().accept(ContentType.JSON)
+                .when().get($URL + "/api/spartans/57");
+
+
+        assertEquals(200, response.statusCode());
+        assertEquals("application/json", response.contentType());
+        assertEquals(ContentType.JSON.toString(), response.header("Content-Type"));
+
+        System.out.println("response.body().asString().contains(\"Freida\") = " + response.body().asString().contains("Freida"));
+
+//      System.out.println(response.body().asString());
+
+        assertTrue(response.body().asString().contains("\"name\":\"Freida\""));
+
+        response.prettyPrint();
+    }
+
+    /*
+    Given no headers provided
+    When Users send GET request to /api/hello
+    Then response status code should be 200
+    And Content header should be "text/plain;charset=UTF-8"
+    And header should contain Date
+    And Content-Length should be 17
+    And body should be "Hello from Sparta"
+     */
+    @DisplayName("hello spartan")
+    @Test
+    public void helloSpartan() {
+        Response response = RestAssured.get($URL + "/api/hello");
+
+        String actualContentType = response.contentType();
+        boolean doesContainDate = response.headers().hasHeaderWithName("Date");
+        int actualContentLength = Integer.parseInt(response.getHeader("Content-Length"));
+
+        assertEquals(200, response.statusCode());
+        assertEquals(ContentType.TEXT+";charset=UTF-8", actualContentType);
+        assertTrue(doesContainDate);
+        assertEquals(17, actualContentLength);
+        assertEquals("Hello from Sparta", response.body().asString());
+
+        System.out.println("response.contentType() = " + response.contentType());
+        System.out.println("response.headers().toString().contains(\"Date\") = " + doesContainDate);
+        System.out.println("response.getBody().asString().length() = " + actualContentLength);
+
+        response.prettyPrint();
     }
 }
