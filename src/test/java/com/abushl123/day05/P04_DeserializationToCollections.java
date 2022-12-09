3624030269a3da6d7b2980b8194bdc4f1c5d2a6a
Abubakr Shoaliev <shoalievabubakr@gmail.com>
2022-12-09 10:58:47 +0500

    Deserialization To Collections

--- src/test/java/com/abushl123/day05/P04_DeserializationToCollections.java
+++ src/test/java/com/abushl123/day05/P04_DeserializationToCollections.java
@@ -1,17 +1,15 @@
 package com.abushl123.day05;
 
-import com.abushl123.utilities.HRTestBase;
 import com.abushl123.utilities.SpartanTestBase;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Map;
 
 import static io.restassured.RestAssured.*;
-
-import static org.hamcrest.MatcherAssert.*;
-import static org.hamcrest.Matchers.*;
 
 
 public class P04_DeserializationToCollections extends SpartanTestBase {
@@ -28,5 +26,45 @@ public class P04_DeserializationToCollections extends SpartanTestBase {
 
         Map<String, Object> spartanMap = response.as(Map.class);
         System.out.println(spartanMap);
+    }
+
+    @Test
+    public void test2() {
+        Response response = given().accept(ContentType.JSON).
+                when().get("/api/spartans").
+                then().statusCode(200).contentType(ContentType.JSON.toString()).extract().response();
+
+        List<Map<String, Object>> listResponse = response.as(List.class);
+
+        for (Map<String, Object> eachMap : listResponse) {
+            System.out.println(eachMap);
+        }
+
+        System.out.println("listResponse.get(0).get(\"id\") = " + listResponse.get(0).get("id"));
+
+        System.out.println("listResponse.get(listResponse.size()-1).get(\"name\") = " + listResponse.get(0).get("name"));
+
+        System.out.println("listResponse.get(0).get(\"gender\") = " + listResponse.get(0).get("gender"));
+
+        System.out.println("listResponse.get(0).get(\"phone\") = " + listResponse.get(0).get("phone"));
+
+        Map<String, Object> mapOf0 = response.path("[0]");
+        System.out.println(mapOf0);
+
+
+        System.out.println("\n\n\n\n-----------------------------------\n\n\n\n");
+        // through jsonPath
+        JsonPath jsonPath = response.jsonPath();
+
+        List<Map<String, Object>> jsonPathList = jsonPath.getList("");
+        for (Map<String, Object> eachMap : jsonPathList) {
+            System.out.println(eachMap);
+        }
+
+        // spartan with index 15 (10+1+4 = 15) (because id of spartans start from 4 and list 0-indexed)
+        System.out.println("jsonPathList.get(10).get(\"id\") = " + jsonPathList.get(10).get("id"));
+        System.out.println("jsonPathList.get(10).get(\"name\") = " + jsonPathList.get(10).get("name"));
+        System.out.println("jsonPathList.get(10).get(\"gender\") = " + jsonPathList.get(10).get("gender"));
+        System.out.println("jsonPathList.get(10).get(\"phone\") = " + jsonPathList.get(10).get("phone"));
     }
 }
