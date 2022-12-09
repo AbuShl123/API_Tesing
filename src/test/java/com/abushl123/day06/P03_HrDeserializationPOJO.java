1bfbcc05b117a7c03cb9987b7eefe3f5f8e69f05
Abubakr Shoaliev <shoalievabubakr@gmail.com>
2022-12-10 02:04:29 +0500

    adding new dependency -- lombok!

---
+++ src/test/java/com/abushl123/day06/P03_HrDeserializationPOJO.java
@@ -0,0 +1,12 @@
+package com.abushl123.day06;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class P03_HrDeserializationPOJO {
+    @DisplayName("Get regions to deserialization to POJO + LOMBOK + ")
+    @Test
+    public void test1() {
+
+    }
+}
---
+++ src/test/java/com/abushl123/pojo/Link.java
@@ -0,0 +1,12 @@
+package com.abushl123.pojo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Link {
+    private String rel;
+    private String href;
+
+}
