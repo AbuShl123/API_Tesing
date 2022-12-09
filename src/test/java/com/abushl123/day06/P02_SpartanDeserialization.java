3cd93e2d0bc5adbf3d7485a22166e8f12fdbebfd
Abubakr Shoaliev <shoalievabubakr@gmail.com>
2022-12-10 01:20:20 +0500

    reaslising pojo even more

--- src/test/java/com/abushl123/day06/P02_SpartanDeserialization.java
+++ src/test/java/com/abushl123/day06/P02_SpartanDeserialization.java
@@ -30,13 +30,39 @@ public class P02_SpartanDeserialization extends SpartanTestBase {
         Spartan spartan = response.as(Spartan.class);
         System.out.println(spartan);
 
+
         System.out.println("spartan.getId() = " + spartan.getId());
         System.out.println("spartan.getName() = " + spartan.getName());
         System.out.println("spartan.getGender() = " + spartan.getGender());
         System.out.println("spartan.getGender() = " + spartan.getPhone());
 
+        System.out.println("----------------SPARTAN_JSONPATH-------------------");
+
         JsonPath jsonPath = response.jsonPath();
         Spartan spartanJsonPath = jsonPath.getObject("", Spartan.class);
         System.out.println("\n" + spartanJsonPath);
+
+        System.out.println("spartanJsonPath.getId() = " + spartanJsonPath.getId());
+        System.out.println("spartanJsonPath.getName() = " + spartanJsonPath.getName());
+        System.out.println("spartanJsonPath.getPhone() = " + spartanJsonPath.getPhone());
+        System.out.println("spartanJsonPath.getGender() = " + spartanJsonPath.getGender());
+    }
+
+    @DisplayName("Get spartans for search endpoints for deserialization the project")
+    @Test
+    public void test2() {
+        Response response = given().accept(ContentType.JSON).
+                when().get("/api/spartans/search").prettyPeek().
+                then().statusCode(200).extract().response();
+
+        System.out.println("-------- RESPONSE get one spartan --------");
+        // response.as(Spartan.class); since we cannot put as here to get specific part of response we are
+        // not going do it
+
+        System.out.println("-------- JSONPATH get one spartan --------");
+        JsonPath jsonPath = response.jsonPath();
+
+        Spartan firstSpartan = jsonPath.getObject("content[0]", Spartan.class);
+        System.out.println(firstSpartan);
     }
 }
