722f4ff5c9939ebe87973f57c027473d7afd3598
Abubakr Shoaliev <shoalievabubakr@gmail.com>
2022-12-13 02:01:43 +0500

    automating post - new more features

--- src/test/java/com/abushl123/day07/P02_SpartanPost.java
+++ src/test/java/com/abushl123/day07/P02_SpartanPost.java
@@ -1,5 +1,6 @@
 package com.abushl123.day07;
 
+import com.abushl123.pojo.Spartan;
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.DisplayName;
@@ -14,6 +15,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class P02_SpartanPost extends SpartanTestBase {
 
+    static String expectedMessage = "A Spartan is Born!";
+
     @DisplayName("POST Spartan with String body")
     @Test
     public void test1() {
@@ -22,8 +25,6 @@ public class P02_SpartanPost extends SpartanTestBase {
                 "  \"name\": \"Mike\",\n" +
                 "  \"phone\": 1234567890\n" +
                 "}";
-
-        String expectedMessage = "A Spartan is Born!";
 
         JsonPath jsonPath = given().accept(ContentType.JSON)  // API send me response in json format
                 .contentType(ContentType.JSON) // API I am sending body in JSON format
@@ -76,8 +77,6 @@ public class P02_SpartanPost extends SpartanTestBase {
                         .contentType(ContentType.JSON)
                         .extract().jsonPath();
 
-        String expectedMessage = "A Spartan is Born!";
-
         assertEquals(expectedMessage, jsonPath.getString("success"));
         assertEquals("James Bond", jsonPath.getString("data.name"));
         assertEquals(9000812312L, jsonPath.getLong("data.phone"));
@@ -86,5 +85,59 @@ public class P02_SpartanPost extends SpartanTestBase {
         System.out.println("id = " + id);
     }
 
+    @Test
+    public void test3() {
+        Spartan requestBody = new Spartan();
+        requestBody.setName("Hercules");
+        requestBody.setGender("Male");
+        requestBody.setPhone(88834911291L);
+
+        System.out.println("requestBody = " + requestBody);
+
+        JsonPath jsonPath =
+                given().accept(ContentType.JSON).log()
+                        .body().contentType(ContentType.JSON)
+                        .body(requestBody)
+                .when().post("/api/spartans").prettyPeek()
+                        .then().statusCode(201).contentType(ContentType.JSON)
+                        .extract().jsonPath();
+
+        assertEquals(expectedMessage, jsonPath.getString("success"));
+        assertEquals(requestBody.getName(), jsonPath.getString("data.name"));
+        assertEquals(requestBody.getGender(), jsonPath.getString("data.gender"));
+        assertEquals(requestBody.getPhone(), jsonPath.getLong("data.phone"));
+
+        int id = jsonPath.getInt("data.id");
+        System.out.println("id = " + id);
+    }
+
+    @Test
+    public void test4() {
+        Spartan requestBody = new Spartan();
+        requestBody.setName("Hercules");
+        requestBody.setGender("Male");
+        requestBody.setPhone(88834911291L);
+
+        System.out.println("requestBody = " + requestBody);
+
+        int idOfNewSpartan = given().accept(ContentType.JSON).log().body()
+                .contentType(ContentType.JSON)
+                .body(requestBody)
+                .when().post("/api/spartans").prettyPeek()
+                .then().statusCode(201).contentType(ContentType.JSON)
+                .extract().jsonPath().getInt("data.id");
+
+        System.out.println("idOfNewSpartan = " + idOfNewSpartan);
+
+        Spartan newSpartan = given().contentType(ContentType.JSON).pathParam("id", idOfNewSpartan)
+                .when().get("/api/spartans/{id}")
+                .then().statusCode(200).contentType(ContentType.JSON)
+                .extract().response().as(Spartan.class);
+
+        assertEquals(requestBody.getName(), newSpartan.getName());
+        assertEquals(requestBody.getGender(), newSpartan.getGender());
+        assertEquals(requestBody.getPhone(), newSpartan.getPhone());
+
+    }
 }
 
