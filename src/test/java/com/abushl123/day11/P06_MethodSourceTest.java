18c4fc8931f66a678f77e57708dc13a434a5319a
Abubakr Shoaliev <shoalievabubakr@gmail.com>
2022-12-21 13:16:55 +0500

    data driven testing with sourceMethod and ExcelUtil

--- src/test/java/com/abushl123/day11/P06_MethodSourceTest.java
+++ src/test/java/com/abushl123/day11/P06_MethodSourceTest.java
@@ -1,10 +1,12 @@
 package com.abushl123.day11;
 
+import com.abushl123.utilities.utils.ExcelUtil;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 public class P06_MethodSourceTest {
 
@@ -13,6 +15,23 @@ public class P06_MethodSourceTest {
     public void test1(String name) {
         System.out.println("name = " + name);
     }
+
+
+    @ParameterizedTest
+    @MethodSource("getExcelData")
+    public void test2(Map<String, String> userInfo) {
+        System.out.println(userInfo);
+        System.out.println("userInfo.get(\"Email\") = " + userInfo.get("Email"));
+        System.out.println("userInfo.get(\"Password\") = " + userInfo.get("Password"));
+        System.out.println("------------");
+    }
+
+
+    public static List<Map<String, String>> getExcelData() {
+        ExcelUtil excelUtil = ExcelUtil.excelUtil("src/test/resources/Library.xlsx", "Library1-short");
+        return excelUtil.getDataList();
+    }
+
 
     public static List<String> getNames() {
         return Arrays.asList("Kimberly", "King", "TJ", "Bond");
